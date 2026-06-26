4e463ef532be34c23da0516008747dce9daaa479
Manya <manya13mehta@gmail.com>
2026-06-26 17:47:09 +0530

    day 4

---
+++ RATED 800/28_1783.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;   
+            arr[i] = x;
+        }
+        sort(arr.begin(), arr.end());
+
+        if (arr[0] == arr[n - 1]) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+            cout << arr[n - 1] << " ";
+            for (int i = 0; i < n - 1; i++)
+                cout << arr[i] << " ";
+            cout << '\n';
+        }
+        
+    }
+}
